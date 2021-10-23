00cc72ec6d834bd4604c6a1b849aebaea1431e51
MincoMK <mail@drchi.co.kr>
2021-10-23 20:57:58 +0900

    ???

--- index.php
+++ index.php
@@ -1,21 +1,15 @@
-<?php
-    // connect heartbeat here
-?>
-
-<?php
-    include("src/php/log.php") // log their ip if they visited the page
-?>
+<!DOCTYPE html>
 <html>
     <!-- External -->
-    <link rel="stylesheet" href="src/css/style.css">
-    <link rel="stylesheet" href="src/css/font-awesome.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
     <script src="src/js/script.js"></script>
         <!-- Cookie Constent -->
         <link rel="stylesheet" href="cookie.css">
-        <script src="src/js/cookie.js"></script>
+        <script src="js/cookie.js"></script>
     
     <!-- Open Graph -->
-    <meta property="og:image" content="src/img/logo.png" />
+    <meta property="og:image" content="img/logo.png" />
     <meta property="og:title" content="Dashboard - DownNotify" />
     <meta property="og:description" content="View DownNotify Dashboard" />
     <meta property="og:site_name" content="www.minco.kro.kr/downnotify/dashboard" />
@@ -26,11 +20,11 @@
     <footer>
      <!-- START Bootstrap-Cookie-Alert -->
     <div class="alert text-center cookiealert" role="alert" style="font-size: 24px; font-family: arial; padding: 20px;">
-    <b>Do you like cookies?</b> &#x1F36A; We use cookies to ensure you to the best experience on our dashboard. <a href="information#cookie" target="_blank">Learn more</a><button type="button" style="border-radius: 8px; border: solid #7289da; background-color: #7289da; color: #fff;" class="btn btn-primary btn-sm acceptcookies">I accept</button>
+    <b>Do you like cookies?</b> We use cookies to ensure you to the best experience on our dashboard. <a href="information#cookie" target="_blank">Learn more</a><button type="button" style="border-radius: 8px; border: solid #7289da; background-color: #7289da; color: #fff;" class="btn btn-primary btn-sm acceptcookies">I accept</button>
     </div>
     <!-- END Bootstrap-Cookie-Alert -->
     <!-- Include cookiealert script -->
-    <script src="src/js/cookie.js"></script>
-    <link rel="stylesheet" href="src/css/cookie.css">
+	<script src="js/cookie.js"></script>
+	<link rel="stylesheet" href="css/cookie.css">
     </footer>
 </html>
