fca34c11e9147ceb21808c4091f512cec9e49faa
Hen2527 <support@hen2527.tk>
2021-10-23 14:05:22 +0800

    Added logs.php and more

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
     // connect heartbeat here
 ?>
+
+<?php
+    include("src/php/log.php") // log their ip if they visited the page
+?>
+<html>
+    <!-- External -->
+    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/font-awesome.css">
+    <script src="src/js/script.js"></script>
+        <!-- Cookie Constent -->
+        <link rel="stylesheet" href="cookie.css">
+        <script src="src/js/cookie.js"></script>
+    
+    <!-- Open Graph -->
+    <meta property="og:image" content="src/img/logo.png" />
+    <meta property="og:title" content="Dashboard - DownNotify" />
+    <meta property="og:description" content="View DownNotify Dashboard" />
+    <meta property="og:site_name" content="www.minco.kro.kr/downnotify/dashboard" />
+    <meta name="theme-color" content="# ADD HEX COLOR HERE WHATEVER YOU WANT" />
+    <meta name="title" content="Dashbord - DownNotify" />
+    <meta name="description" content="View DownNotify Dashboard" />
+</html>
